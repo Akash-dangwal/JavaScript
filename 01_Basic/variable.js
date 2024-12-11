5dbcc5d2c393fcf6c2da9af090da47da85ed0f3c
Akash-dangwal <154399788+Akash-dangwal@users.noreply.github.com>
2024-12-11 17:35:52 +0000

    day2

---
+++ 01_Basic/variable.js
@@ -0,0 +1,8 @@
+const id=2334;
+let name="abcd"
+var mail="abc@m.com" //scope problem 
+city="mp"
+
+// id=1122;//not possible...
+console.table({id,name,mail,city});
+//prefer to use const and let...
